7b37bc33a188ad9fdc825852972f2ebc6a872238
Holger Staudacher <holger.staudacher@gmail.com>
2020-11-20 21:28:57 +0100

    Round m conversion to next 10

--- modules/components/options/FromOption.ts
+++ modules/components/options/FromOption.ts
@@ -5,13 +5,18 @@ export abstract class FromOption {
   constructor(readonly title: string, readonly icon: string, readonly type: string, readonly color: string) {}
 
   private format = (value: number, unit: string): string => {
-    console.log(unit);
     if (value < 1 && value > 0 && unit !== 'cal') {
       return numeral(value).format('0,0.00');
     } else if (unit === 'cal' || unit === 'lbs' || unit === 'kg') {
       return numeral(value).format();
+    } else if (unit === 'm') {
+      return numeral(this.roundToNearestTen(value)).format();
     }
     return numeral(value).format('0,0');
+  };
+
+  private roundToNearestTen = (value: number): number => {
+    return Math.round(value / 10) * 10;
   };
 
   convert = (configuration: ConversionConfiguration): Array<ConversionResult> => {
