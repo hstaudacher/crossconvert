e895734b1a09b666c18107bb03fd1bb50c8afebb
Holger Staudacher <holger.staudacher@gmail.com>
2021-01-02 21:30:48 +0100

    Formating weight percentages

--- modules/components/WeightPercentagesScreen.tsx
+++ modules/components/WeightPercentagesScreen.tsx
@@ -6,6 +6,7 @@ import {ListItem} from 'react-native-elements';
 import {NavigationComponentProps} from 'react-native-navigation';
 import ConversionConfiguration from './ConversionConfiguration';
 import {WeightPercentager, WeightPercentage} from './options/WeightPercentager';
+import Color from 'color';
 
 interface WeightDetailsScreenProperties extends NavigationComponentProps {
   configuration: ConversionConfiguration;
@@ -15,16 +16,32 @@ const formatUnit = (unit: WeightUnit): string => {
   return unit === WeightUnit.kg ? 'kg' : 'lbs';
 };
 
+const computeFontColor = (percentage: number): string => {
+  if (percentage > 100) {
+    return Color('#ff6347')
+      .lighten((percentage - 100) / 200)
+      .hex()
+      .toString();
+  }
+  if (percentage < 100) {
+    return Color('#ff6347')
+      .lighten((100 - percentage) / 200)
+      .hex()
+      .toString();
+  }
+  return '#ff4500';
+};
+
 const renderItem = (percentage: WeightPercentage) => {
   return (
     <>
       <ListItem bottomDivider>
-        <Text style={{fontSize: 30, color: 'tomato'}}>{percentage.percentage}%</Text>
+        <Text style={{fontSize: 30, color: computeFontColor(percentage.percentage)}}>{percentage.percentage}%</Text>
         <ListItem.Content style={{alignItems: 'flex-end'}}>
-          <ListItem.Title style={{fontSize: 25}}>
+          <ListItem.Title style={{fontSize: 25, opacity: 0.9}}>
             {percentage.conversion.fromWeight} {formatUnit(percentage.conversion.fromUnit)}
           </ListItem.Title>
-          <ListItem.Subtitle>
+          <ListItem.Subtitle style={{opacity: 0.6}}>
             {percentage.conversion.toWeight} {formatUnit(percentage.conversion.toUnit)}
           </ListItem.Subtitle>
         </ListItem.Content>
