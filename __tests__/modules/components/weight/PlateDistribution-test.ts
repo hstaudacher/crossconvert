5b609683630f93ad79cefacb2f3d738727eee928
Holger Staudacher <holger.staudacher@gmail.com>
2021-01-08 14:13:37 +0100

    Finish plate distribution tests

--- __tests__/modules/components/weight/PlateDistribution-test.ts
+++ __tests__/modules/components/weight/PlateDistribution-test.ts
@@ -31,7 +31,50 @@ describe('plate distribution uses color coded plates', () => {
   test('distributes 2 fractional white kgs', () =>
     assertPlateDistribution([0.5, 0.5], WeightUnit.kg, [new PlateGroup(Plate.FRACTIONAL_WHITE, 2)]));
 
-  // TODO: add lbs tests + tests for multi plates + plate sorting (high to low)
+  test('distributes 2 blue and 2 fractional green kgs', () =>
+    assertPlateDistribution([20, 20, 1, 1], WeightUnit.kg, [
+      new PlateGroup(Plate.BLUE, 2),
+      new PlateGroup(Plate.FRACTIONAL_GREEN, 2),
+    ]));
+
+  test('distributes weights sorted', () =>
+    assertPlateDistribution([1, 1, 20, 20], WeightUnit.kg, [
+      new PlateGroup(Plate.BLUE, 2),
+      new PlateGroup(Plate.FRACTIONAL_GREEN, 2),
+    ]));
+
+  test('distributes 2 red lbs', () =>
+    assertPlateDistribution([55, 55], WeightUnit.lbs, [new PlateGroup(Plate.RED, 2)]));
+
+  test('distributes 2 blue lbs', () =>
+    assertPlateDistribution([45, 45], WeightUnit.lbs, [new PlateGroup(Plate.BLUE, 2)]));
+
+  test('distributes 2 yellow lbs', () =>
+    assertPlateDistribution([35, 35], WeightUnit.lbs, [new PlateGroup(Plate.YELLOW, 2)]));
+
+  test('distributes 2 green lbs', () =>
+    assertPlateDistribution([25, 25], WeightUnit.lbs, [new PlateGroup(Plate.GREEN, 2)]));
+
+  test('distributes 2 white lbs', () =>
+    assertPlateDistribution([10, 10], WeightUnit.lbs, [new PlateGroup(Plate.WHITE, 2)]));
+
+  test('distributes 2 fractional blue lbs', () =>
+    assertPlateDistribution([5, 5], WeightUnit.lbs, [new PlateGroup(Plate.FRACTIONAL_BLUE, 2)]));
+
+  test('distributes 2 fractional green lbs', () =>
+    assertPlateDistribution([2.5, 2.5], WeightUnit.lbs, [new PlateGroup(Plate.FRACTIONAL_GREEN, 2)]));
+
+  test('distributes 2 fractional red lbs', () =>
+    assertPlateDistribution([1, 1], WeightUnit.lbs, [new PlateGroup(Plate.FRACTIONAL_RED, 2)]));
+
+  test('distributes 2 fractional yellow lbs', () =>
+    assertPlateDistribution([0.5, 0.5], WeightUnit.lbs, [new PlateGroup(Plate.FRACTIONAL_YELLOW, 2)]));
+
+  test('distributes 2 blues and 2 fractional green lbs', () =>
+    assertPlateDistribution([45, 45, 2.5, 2.5], WeightUnit.lbs, [
+      new PlateGroup(Plate.BLUE, 2),
+      new PlateGroup(Plate.FRACTIONAL_GREEN, 2),
+    ]));
 });
 
 const assertPlateDistribution = (weights: number[], unit: WeightUnit, expectedGroups: PlateGroup[]): void => {
