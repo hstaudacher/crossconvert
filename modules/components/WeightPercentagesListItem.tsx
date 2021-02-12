fc8572ae2e1b20a23266082d6a3f0d2c1dd2c35a
Holger Staudacher <holger.staudacher@gmail.com>
2021-02-12 17:42:05 +0100

    Refactor out percentage list item

---
+++ modules/components/WeightPercentagesListItem.tsx
@@ -0,0 +1,137 @@
+/* eslint-disable react-native/no-inline-styles */
+import Color from 'color';
+import {WeightUnit} from '../conversion';
+import React, {useState} from 'react';
+import {View} from 'react-native';
+import {Icon, ListItem, Overlay, Text} from 'react-native-elements';
+import {PlateMapping} from './settings/Plates';
+import {WeightSettings} from './settings/WeightSettings';
+import {PlateDistribution, PlateDistributor} from './weight/PlateDistributor';
+import {renderPlateIcon} from './weight/PlateVisualization';
+import {WeightPercentage} from './weight/WeightPercentager';
+import WeightPlatesComponent from './WeightPlatesComponent';
+
+interface WeightPercentageListItemProperties {
+  percentage: WeightPercentage;
+  settings: WeightSettings;
+}
+
+const formatUnit = (unit: WeightUnit): string => {
+  return unit === WeightUnit.kg ? 'kg' : 'lbs';
+};
+
+const computeFontColor = (percentage: number): string => {
+  if (percentage > 100) {
+    return Color('#ff6347')
+      .lighten((percentage - 100) / 200)
+      .hex()
+      .toString();
+  }
+  if (percentage < 100) {
+    return Color('#ff6347')
+      .lighten((100 - percentage) / 200)
+      .hex()
+      .toString();
+  }
+  return '#ff4500';
+};
+
+const getWeightText = (mapping: PlateMapping, unit: WeightUnit) => {
+  if (unit === WeightUnit.kg) {
+    return mapping.kg + 'kg';
+  }
+  return mapping.lbs + 'lbs';
+};
+
+const WeightPercentagesListItem = (props: WeightPercentageListItemProperties) => {
+  const [legendVisible, setLegendVisible] = useState(false);
+
+  const [distribution, setDistribution] = useState(new PlateDistribution([], WeightUnit.kg));
+
+  const toggleLegend = (percentage: WeightPercentage) => {
+    setDistribution(getPlates(percentage.conversion.fromWeight, percentage.conversion.fromUnit));
+    setLegendVisible(!legendVisible);
+  };
+
+  const getPlates = (weight: number, unit: WeightUnit): PlateDistribution => {
+    const distributor = new PlateDistributor(props.settings, unit);
+    return distributor.getPlateDistribution(weight);
+  };
+
+  const renderCantDistribute = () => {
+    if (!distribution.completelyDistributed) {
+      return (
+        <View style={{flexDirection: 'row', marginBottom: 10, marginHorizontal: 10}} key={'na'}>
+          <Icon
+            key={'na'}
+            name={'exclamation-triangle'}
+            type={'font-awesome'}
+            color={'tomato'}
+            size={25}
+            containerStyle={{alignSelf: 'flex-end', width: 40}}
+            reverse={false}
+          />
+          <Text h4 style={{alignSelf: 'center', marginLeft: 8, color: 'tomato'}}>
+            Not enough plates
+          </Text>
+        </View>
+      );
+    }
+  };
+
+  return (
+    <>
+      <ListItem bottomDivider onPress={() => toggleLegend(props.percentage)}>
+        <Text style={{fontSize: 30, color: computeFontColor(props.percentage.percentage)}}>
+          {props.percentage.percentage}%
+        </Text>
+        <ListItem.Content style={{alignItems: 'flex-end'}}>
+          <ListItem.Title style={{fontSize: 25, opacity: 0.9}}>
+            <Text>
+              {props.percentage.conversion.fromWeight}
+              {formatUnit(props.percentage.conversion.fromUnit)}
+            </Text>
+            <Text style={{fontSize: 20, opacity: 0.6}}> / </Text>
+            <Text style={{fontSize: 20, opacity: 0.6}}>
+              {props.percentage.conversion.toWeight}
+              {formatUnit(props.percentage.conversion.toUnit)}
+            </Text>
+          </ListItem.Title>
+          <ListItem.Subtitle style={{opacity: 0.9, marginTop: 4}}>
+            <View>
+              <WeightPlatesComponent
+                plateDistribution={getPlates(
+                  props.percentage.conversion.fromWeight,
+                  props.percentage.conversion.fromUnit,
+                )}
+              />
+            </View>
+          </ListItem.Subtitle>
+          <Overlay
+            isVisible={legendVisible}
+            onBackdropPress={() => toggleLegend(props.percentage)}
+            overlayStyle={{borderRadius: 5}}>
+            <View style={{paddingTop: 10}}>
+              {renderCantDistribute()}
+              {distribution.getPlateGroups().map((group) => (
+                <View
+                  style={{flexDirection: 'row', marginBottom: 10, marginHorizontal: 10}}
+                  key={group.mapping.plate.toString()}>
+                  {renderPlateIcon(group.mapping.plate, {alignSelf: 'flex-end', width: 40})}
+                  <Text h4 style={{alignSelf: 'center', marginLeft: 8, color: 'tomato'}}>
+                    {group.amount}x{' '}
+                  </Text>
+                  <Text h4 style={{alignSelf: 'center'}}>
+                    {getWeightText(group.mapping, props.percentage.conversion.fromUnit)}
+                  </Text>
+                </View>
+              ))}
+            </View>
+          </Overlay>
+        </ListItem.Content>
+      </ListItem>
+    </>
+  );
+};
+
+export default WeightPercentagesListItem;
--- modules/components/WeightPercentagesScreen.tsx
+++ modules/components/WeightPercentagesScreen.tsx
@@ -1,52 +1,20 @@
 /* eslint-disable react-native/no-inline-styles */
-import {WeightUnit} from '../conversion';
-import React, {useEffect, useRef, useState} from 'react';
-import {View, StyleSheet, Dimensions, Animated, TouchableOpacity} from 'react-native';
-import {Icon, ListItem, Overlay, Text} from 'react-native-elements';
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import {Text} from 'react-native-elements';
 import {Navigation, NavigationComponentProps} from 'react-native-navigation';
 import ConversionConfiguration from './ConversionConfiguration';
-import {WeightPercentager, WeightPercentage} from './weight/WeightPercentager';
-import Color from 'color';
-import {PlateDistributor, PlateDistribution} from './weight/PlateDistributor';
-import WeightPlatesComponent from './WeightPlatesComponent';
-import {renderPlateIcon} from './weight/PlateVisualization';
-import {PlateMapping} from './settings/Plates';
+import {WeightPercentager} from './weight/WeightPercentager';
 import {WeightSettingsStore} from './settings/WeightSettings';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {SwipeListView} from 'react-native-swipe-list-view';
+import WeightPercentagesListItem from './WeightPercentagesListItem';
 
-interface WeightDetailsScreenProperties extends NavigationComponentProps {
+interface WeightPercentagesScreenProperties extends NavigationComponentProps {
   configuration: ConversionConfiguration;
 }
 
-const formatUnit = (unit: WeightUnit): string => {
-  return unit === WeightUnit.kg ? 'kg' : 'lbs';
-};
-
-const computeFontColor = (percentage: number): string => {
-  if (percentage > 100) {
-    return Color('#ff6347')
-      .lighten((percentage - 100) / 200)
-      .hex()
-      .toString();
-  }
-  if (percentage < 100) {
-    return Color('#ff6347')
-      .lighten((100 - percentage) / 200)
-      .hex()
-      .toString();
-  }
-  return '#ff4500';
-};
-
-const getWeightText = (mapping: PlateMapping, unit: WeightUnit) => {
-  if (unit === WeightUnit.kg) {
-    return mapping.kg + 'kg';
-  }
-  return mapping.lbs + 'lbs';
-};
-
-const setupSettingsButton = (props: WeightDetailsScreenProperties) => {
+const setupSettingsButton = (props: WeightPercentagesScreenProperties) => {
   const onSettingsPress = () => {
     Navigation.push(props.componentId, {
       component: {
@@ -88,110 +56,20 @@ const setupSettingsButton = (props: WeightDetailsScreenProperties) => {
 
 const store = new WeightSettingsStore();
 
-const WeightPercentagesScreen = (props: WeightDetailsScreenProperties) => {
+const WeightPercentagesScreen = (props: WeightPercentagesScreenProperties) => {
   setupSettingsButton(props);
 
   const [weightSettings, setWeightSettings] = useState(store.defaultSettings);
+  const [percentages, setPercentages] = useState([110, 100, 85]); // TODO: persist
+
   useEffect(() => {
     const subscription = Navigation.events().registerComponentDidAppearListener((event) => {
       if (props.componentId === event.componentId) {
         store.load().then((s) => setWeightSettings(s));
       }
     });
-
     return () => subscription.remove();
   });
-
-  const getPlates = (weight: number, unit: WeightUnit): PlateDistribution => {
-    const distributor = new PlateDistributor(weightSettings, unit);
-    return distributor.getPlateDistribution(weight);
-  };
-
-  const [legendVisible, setLegendVisible] = useState(false);
-
-  const toggleLegend = (percentage: WeightPercentage) => {
-    setDistribution(getPlates(percentage.conversion.fromWeight, percentage.conversion.fromUnit));
-    setLegendVisible(!legendVisible);
-  };
-
-  const [distribution, setDistribution] = useState(new PlateDistribution([], WeightUnit.kg));
-
-  const renderItem = (percentage: WeightPercentage) => {
-    const renderCantDistribute = () => {
-      if (!distribution.completelyDistributed) {
-        return (
-          <View style={{flexDirection: 'row', marginBottom: 10, marginHorizontal: 10}} key={'na'}>
-            <Icon
-              key={'na'}
-              name={'exclamation-triangle'}
-              type={'font-awesome'}
-              color={'tomato'}
-              size={25}
-              containerStyle={{alignSelf: 'flex-end', width: 40}}
-              reverse={false}
-            />
-            <Text h4 style={{alignSelf: 'center', marginLeft: 8, color: 'tomato'}}>
-              Not enough plates
-            </Text>
-          </View>
-        );
-      }
-    };
-
-    return (
-      <>
-        <ListItem bottomDivider onPress={() => toggleLegend(percentage)}>
-          <Text style={{fontSize: 30, color: computeFontColor(percentage.percentage)}}>{percentage.percentage}%</Text>
-          <ListItem.Content style={{alignItems: 'flex-end'}}>
-            <ListItem.Title style={{fontSize: 25, opacity: 0.9}}>
-              <Text>
-                {percentage.conversion.fromWeight}
-                {formatUnit(percentage.conversion.fromUnit)}
-              </Text>
-              <Text style={{fontSize: 20, opacity: 0.6}}> / </Text>
-              <Text style={{fontSize: 20, opacity: 0.6}}>
-                {percentage.conversion.toWeight}
-                {formatUnit(percentage.conversion.toUnit)}
-              </Text>
-            </ListItem.Title>
-            <ListItem.Subtitle style={{opacity: 0.9, marginTop: 4}}>
-              <View>
-                <WeightPlatesComponent
-                  plateDistribution={getPlates(percentage.conversion.fromWeight, percentage.conversion.fromUnit)}
-                />
-              </View>
-            </ListItem.Subtitle>
-            <Overlay
-              isVisible={legendVisible}
-              onBackdropPress={() => toggleLegend(percentage)}
-              overlayStyle={{borderRadius: 5}}>
-              <View style={{paddingTop: 10}}>
-                {renderCantDistribute()}
-                {distribution.getPlateGroups().map((group) => (
-                  <View
-                    style={{flexDirection: 'row', marginBottom: 10, marginHorizontal: 10}}
-                    key={group.mapping.plate.toString()}>
-                    {renderPlateIcon(group.mapping.plate, {alignSelf: 'flex-end', width: 40})}
-                    <Text h4 style={{alignSelf: 'center', marginLeft: 8, color: 'tomato'}}>
-                      {group.amount}x{' '}
-                    </Text>
-                    <Text h4 style={{alignSelf: 'center'}}>
-                      {getWeightText(group.mapping, percentage.conversion.fromUnit)}
-                    </Text>
-                  </View>
-                ))}
-              </View>
-            </Overlay>
-          </ListItem.Content>
-        </ListItem>
-      </>
-    );
-  };
-
-  const [percentages, setPercentages] = useState([110, 100, 85]);
-
-  const weightPercentager = new WeightPercentager(props.configuration);
-  const weightPercentages = weightPercentager.getPercentages(percentages);
 
   const closePercentageMenu = (row: any, rows: any) => {
     if (rows[row.index]) {
@@ -217,6 +95,9 @@ const WeightPercentagesScreen = (props: WeightDetailsScreenProperties) => {
     </View>
   );
 
+  const weightPercentager = new WeightPercentager(props.configuration);
+  const weightPercentages = weightPercentager.getPercentages(percentages);
+
   return (
     <>
       <SafeAreaView style={styles.view}>
@@ -224,7 +105,7 @@ const WeightPercentagesScreen = (props: WeightDetailsScreenProperties) => {
           disableRightSwipe
           keyExtractor={(e, i) => i.toString()}
           data={weightPercentages}
-          renderItem={(info) => renderItem(info.item)}
+          renderItem={(info) => <WeightPercentagesListItem percentage={info.item} settings={weightSettings} />}
           renderHiddenItem={renderHiddenItem}
           leftOpenValue={75}
           rightOpenValue={-75}
