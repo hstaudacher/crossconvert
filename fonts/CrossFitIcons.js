df7b262d47cdfbcf49952d0e4178c0fe1bc6c6b0
Holger Staudacher <holger.staudacher@gmail.com>
2021-05-06 17:18:11 +0200

    Update to react native 0.64.1

--- fonts/CrossFitIcons.js
+++ fonts/CrossFitIcons.js
@@ -1,6 +1,11 @@
+import {Platform} from 'react-native';
 import {createIconSetFromIcoMoon} from 'react-native-vector-icons';
 import icoMoonConfig from './CrossFitIcons-selection.json';
 
-const CrossFitIcons = createIconSetFromIcoMoon(icoMoonConfig, 'crossfit', 'CrossFit.ttf');
+const isIOS = Platform.OS === 'ios';
+
+const CrossFitIcons = isIOS
+  ? createIconSetFromIcoMoon(icoMoonConfig)
+  : createIconSetFromIcoMoon(icoMoonConfig, 'crossfit', 'CrossFit.ttf');
 
 export default CrossFitIcons;
