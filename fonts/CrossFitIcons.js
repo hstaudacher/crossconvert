fd743c066f602141fc88512c92ac470bbb3598da
Holger Staudacher <holger.staudacher@gmail.com>
2021-05-06 16:35:43 +0200

    Make app runnable on android

--- fonts/CrossFitIcons.js
+++ fonts/CrossFitIcons.js
@@ -1,6 +1,6 @@
 import {createIconSetFromIcoMoon} from 'react-native-vector-icons';
 import icoMoonConfig from './CrossFitIcons-selection.json';
 
-const CrossFitIcons = createIconSetFromIcoMoon(icoMoonConfig);
+const CrossFitIcons = createIconSetFromIcoMoon(icoMoonConfig, 'crossfit', 'CrossFit.ttf');
 
 export default CrossFitIcons;
